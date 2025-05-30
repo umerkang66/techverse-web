8195e227935832076ad1fef470471fa543c9dd20
Muhammad Umer <umerkang66@gmail.com>
2025-05-30 18:20:16 +0500

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,8 +18,6 @@ app.use((err, req, res, next) => {
     return res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
-      error: err,
-      stack: err.stack,
     });
   }
 
