e1d40b7f5aabe0e8c16e55dd3228eaabba95ab4f
Muhammad Umer <umerkang66@gmail.com>
2025-05-30 15:44:50 +0500

    started building frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-center text-blue-400">
+        Muhammad Umer and Muhammad Saad
+      </div>
+    </>
+  );
+}
+
+export default App;
